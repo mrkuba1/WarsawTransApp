9c1426519b7de3ee5d3ddce8abcf34b8a7ee8320
mmotrenk <01178675@pw.edu.pl>
2024-04-08 20:59:52 +0200

    lines button add

--- warsawtransapp/lib/pages/home.dart
+++ warsawtransapp/lib/pages/home.dart
@@ -11,6 +11,8 @@ import 'package:warsawtransapp/data/timetables/models/busroutes.dart';
 import 'package:warsawtransapp/widgets/nav_widget.dart';
 import 'package:warsawtransapp/widgets/search_widget.dart';
 
+import '../widgets/lines_button_widget.dart';
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -45,7 +47,36 @@ class _HomeState extends State<Home> {
       print("Wystąpił błąd podczas pobierania danych o przystankach : $e");
     }
   }
+  List<BusRoute> mergeBusRoute(List<Busroutes> busRoutesList) {
+    List<BusRoute> selectedBusRoute = [];
+    for (int i = 0; i < busRoutesList.length; i++) {
+      for (int j = 0; j < busRoutesList[i].routes.length; j++) {
+        busRoutesList[i].routes[j].setBusLine(busRoutesList[i].line as BusLine);
+        selectedBusRoute.add(busRoutesList[i].routes[j]);
+      }
+    }
+    BusRoute.sortBusRoutesByTime(selectedBusRoute);
+    return selectedBusRoute;
+  }
+  Future<void> fetchBusroutes(List<BusLine> lines) async {
+    print(lines);
+    try {
+      for (int i = 0; i < lines.length; i++) {
+        Busroutes busRoutes = await _busClient.getBusRoutes(
+            selectedBusStop!.id, selectedBusStop!.smallid, lines[i].value);
 
+        busRoutes.setBusLine(lines[i]);
+        busRoutesList.add(busRoutes);
+      }
+    } catch (e) {
+      print("Wystąpił błąd podczas pobierania danych: $e");
+    }
+    setState(() {
+      busroutes = mergeBusRoute(busRoutesList);
+    });
+    busRoutesList = [];
+    // print(busroutes);
+  }
   @override
   void initState() {
     super.initState();
@@ -123,10 +154,16 @@ class _HomeState extends State<Home> {
                 SizedBox(
                   width: MediaQuery.of(context).size.width / 4,
                   height: MediaQuery.of(context).size.height / (4 / 3),
+                  child: busstopLines != null && busstopLines!.lines.isNotEmpty
+                      ? LinesButtonsWidget(
+                      items: busstopLines!.lines,
+                      onLinesSelected: fetchBusroutes)
+                      : const SizedBox(), // Jeśli nie ma linii autobusowych, nie wyświetlaj widgetu
                 ),
                 SizedBox(
                   width: MediaQuery.of(context).size.width / (4 / 3),
                   height: MediaQuery.of(context).size.height / (4 / 3),
+
                 )
               ],
             ),
--- warsawtransapp/lib/widgets/lines_button_widget.dart
+++ warsawtransapp/lib/widgets/lines_button_widget.dart
@@ -1,66 +1,93 @@
+
+
+
 import 'package:flutter/material.dart';
 
-class LinesButtonsWidget extends StatefulWidget {
-  final List<String> items;
+import '../data/lines/models/busline.dart';
 
-  const LinesButtonsWidget({super.key, required this.items});
+  class LinesButtonsWidget extends StatefulWidget {
+  final List<BusLine> items;
+  final Function(List<BusLine>) onLinesSelected;
+
+  const LinesButtonsWidget({
+  Key? key,
+  required this.items,
+  required this.onLinesSelected,
+  }) : super(key: key);
 
   @override
   _LinesButtonsWidgetState createState() => _LinesButtonsWidgetState();
-}
 
-class _LinesButtonsWidgetState extends State<LinesButtonsWidget> {
+  static of(BuildContext context) {}
+  }
+
+  class _LinesButtonsWidgetState extends State<LinesButtonsWidget> {
   late List<Color> containerColors;
   late List<bool> isToggled;
 
   @override
   void initState() {
-    super.initState();
-    containerColors = List.filled(widget.items.length - 1, Colors.blue[100]!);
-    isToggled = List.filled(widget.items.length - 1, false);
+  super.initState();
+  containerColors = List.filled(widget.items.length - 1, Colors.blue[100]!);
+  isToggled = List.filled(widget.items.length - 1, false);
+  }
+
+  List<BusLine> getSelectedLines() {
+  List<BusLine> selectedLines = [];
+  for (int i = 0; i < isToggled.length; i++) {
+  if (isToggled[i]) {
+  selectedLines.add(widget.items[i + 1]);
+  }
+  }
+  return selectedLines;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: GridView.builder(
-        itemCount: widget.items.length - 1, // Liczba elementów w siatce
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 5, // Liczba kolumn w siatce
-          crossAxisSpacing: 10.0, // Odległość pomiędzy kolumnami
-          mainAxisSpacing: 10.0, // Odległość pomiędzy wierszami
-          childAspectRatio: 0.5 / 0.5, // Proporcja szerokość / wysokość
-        ),
-        itemBuilder: (BuildContext context, int index) {
-          // Tworzenie i zwracanie elementu siatki
-          return GestureDetector(
-            onTap: () {
-              // Obsługa kliknięcia
-              setState(() {
-                if (isToggled[index]) {
-                  containerColors[index] =
-                      Colors.blue[100]!; // Przywracamy pierwotny kolor
-                } else {
-                  containerColors[index] =
-                      Colors.blueAccent; // Zmieniamy kolor na czerwony
-                }
-                isToggled[index] =
-                    !isToggled[index]; // Odwracamy stan wciśnięcia
-              });
-            },
-            child: Container(
-              color: containerColors[index],
-              child: Center(
-                child: Text(
-                  widget.items[index + 1],
-                  style: const TextStyle(fontSize: 15.0),
-                ),
-              ),
-            ),
-          );
-        },
-      ),
-    );
+  return Padding(
+  padding: const EdgeInsets.all(20.0),
+  child: GridView.builder(
+  itemCount: widget.items.length - 1, // Liczba elementów w siatce
+  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+  crossAxisCount: 5, // Liczba kolumn w siatce
+  crossAxisSpacing: 10.0, // Odległość pomiędzy kolumnami
+  mainAxisSpacing: 10.0, // Odległość pomiędzy wierszami
+  childAspectRatio: 0.5 / 0.5, // Proporcja szerokość / wysokość
+  ),
+  itemBuilder: (BuildContext context, int index) {
+  // Tworzenie i zwracanie elementu siatki
+  return GestureDetector(
+  onTap: () {
+  // Obsługa kliknięcia
+  setState(() {
+  if (isToggled[index]) {
+  containerColors[index] =
+  Colors.blue[100]!; // Przywracamy pierwotny kolor
+  } else {
+  containerColors[index] =
+  Colors.blueAccent; // Zmieniamy kolor na czerwony
   }
-}
+  isToggled[index] =
+  !isToggled[index]; // Odwracamy stan wciśnięcia
+
+  widget.onLinesSelected(getSelectedLines());
+  // print(getSelectedLines());
+  });
+  },
+  child: Container(
+  color: containerColors[index],
+  child: Center(
+  child: Text(
+  widget.items[index + 1].value,
+  style: const TextStyle(fontSize: 15.0),
+  ),
+  ),
+  ),
+  );
+  },
+  ),
+  );
+  }
+  }
+
+
